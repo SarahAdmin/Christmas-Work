4c5cc8047f8dda17cc02f9748f21d2ab14a7ea90
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2025-12-03 21:35:19 +0000

    Add employee data and print DataFrame

---
+++ job_data.py
@@ -0,0 +1,5 @@
+import pandas as pd
+data = {'Name':['Lee Cox','Matt Bond','Lemon Jelly','Pamela Green','Jonah Parker','Zoe Baxter'],"Position":['Seasonal Sales Assistant','Seasonal Sales Assistant','Manager','E-Commerce Assistant','Marketing Assistant',
+                                                                                                           'Assistant Manager'],"Salary",[8000,8000,60000,20000,35000,55000]}
+ChristmasEmployees = pd.DataFrame(data,index=[1,2,3,4,5,6]) 
+print(ChristmasEmployees.to_string())
